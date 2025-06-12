6e72c26e91bc540deea1c887d7523e0c83977bac
Saajvatz <ajvatzalejandro@gmail.com>
2025-06-12 16:54:56 -0600

    eleccion_de_juegos

--- componentes/header/elector/elector.js
+++ componentes/header/elector/elector.js
@@ -1,28 +1,4 @@
-export function createHeader() {
-    const header = document.createElement("header");
-    header.classList.add("main-header");
-
-    const container = document.createElement("div");
-    container.classList.add("header-container");
-
-    // Crear el título
-    const title = document.createElement("h1");
-    title.textContent = "Juego De Equipo";
-
-    // Crear la imagen
-    const img = document.createElement("img");
-    img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq-IciWxLW5lsM2Oco4VluWyfOBSmgukVu6w&s"; // Asegúrate de cambiar la ruta según tu proyecto
-    img.alt = "Logo";
-    img.classList.add("header-logo");
-
-    // Agregar los elementos al contenedor
-    container.appendChild(title);
-    container.appendChild(img);
-
-    // Agregar el contenedor al header
-    header.appendChild(container);
-
-    // Crear botón de menú hamburguesa
+// Crear botón de menú hamburguesa
 const toggleBtn = document.createElement("div");
 toggleBtn.id = "menu-toggle";
 toggleBtn.innerHTML = "<span></span><span></span><span></span>";
@@ -130,6 +106,3 @@ juegos.forEach(nombre => {
   };
   juegosCentrales.appendChild(btn);
 });
-
-    return header;
-}
